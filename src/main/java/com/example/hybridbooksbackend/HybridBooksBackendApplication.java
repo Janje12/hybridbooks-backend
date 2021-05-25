273f357144c4f975839dec1d45a078549578a22d
Janje12 <59922311+Janje12@users.noreply.github.com>
2021-05-25 18:15:05 +0200

    init commit

---
+++ src/main/java/com/example/hybridbooksbackend/HybridBooksBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hybridbooksbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridBooksBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HybridBooksBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hybridbooksbackend/HybridBooksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hybridbooksbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridBooksBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
